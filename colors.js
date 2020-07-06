63d900e82c080672ef09dddeb950f0b3c22b185e
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-06 16:40:47 -0400

    basic working files

---
+++ colors.js
@@ -0,0 +1,41 @@
+
+const baseCoat = [
+    {
+        id: "snow",
+        base: [255, 255, 255],
+        shade: [211, 213, 219],
+        line: [202, 188, 193],
+    },
+    {
+        id: "ivory",
+        base: [247,238,241],
+        shade: [202,188,193],
+        line: [174,147,156],
+    },
+    {
+        id: "vanilla",
+        base: [255,232,215],
+        shade: [228,183,172],
+        line: [204,150,150],
+    },
+    {
+        id: "peach",
+        base: [255, 215, 224],
+        shade: [235, 173, 187],
+        line: [216, 125, 145],
+    },
+    {
+        id: "dusty",
+        base: [220, 189, 189],
+        shade: [180, 149, 149],
+        line: [151, 106, 115],
+    },
+    {
+        id: "creamsicle",
+        base: [255, 173, 134],
+        shade: [255, 131, 116],
+        line: [231, 97, 97],
+    },
+]
+
+const colors = ["snow", "ivory", "vanilla", "peach", "dusty", "creamsicle"]
