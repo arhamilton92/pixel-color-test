4b677d1e0f0b5dcf3413bc48a5b0f956fe8672fe
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-06 20:51:47 -0400

    added more colors

--- colors.js
+++ colors.js
@@ -36,6 +36,60 @@ const baseCoat = [
         shade: [255, 131, 116],
         line: [231, 97, 97],
     },
+    {
+        id: "jam",
+        base: [143, 13, 81],
+        shade: [112, 0, 59],
+        line: [77, 1, 41],
+    },
+    {
+        id: "bubblegum",
+        base: [255, 125, 194],
+        shade: [228, 84, 160],
+        line: [209, 33, 125],
+    },
+    {
+        id: "wine",
+        base: [100, 0, 53],
+        shade: [50, 0, 26],
+        line: [50, 0, 26],
+    },
+    {
+        id: "strawberry",
+        base: [240, 59, 78],
+        shade: [174, 3, 57],
+        line: [112, 0, 59],
+    },
+    {
+        id: "grape jelly",
+        base: [84, 67, 147],
+        shade: [59, 43, 117],
+        line: [42, 28, 95],
+    },
+    {
+        id: "lavender",
+        base: [200, 200, 238],
+        shade: [151, 140, 194],
+        line: [120, 106, 173],
+    },
+    {
+        id: "dandelion",
+        base: [251, 255, 188],
+        shade: [225, 194, 142],
+        line: [199, 144, 105],
+    },
+    {
+        id: "seafoam",
+        base: [88, 29, 188],
+        shade: [52, 179, 186],
+        line: [33, 121, 173],
+    },
+    {
+        id: "sky",
+        base: [183, 244, 255],
+        shade: [100, 195, 229],
+        line: [73,135, 220],
+    },
 ]
 
-const colors = ["snow", "ivory", "vanilla", "peach", "dusty", "creamsicle"]
+const colors = ["snow", "ivory", "vanilla", "peach", "dusty", "creamsicle", "jam", "bubblegum", "wine", "strawberry", "grape jelly", "lavender", "dandelion", "seafoam", "sky"]
